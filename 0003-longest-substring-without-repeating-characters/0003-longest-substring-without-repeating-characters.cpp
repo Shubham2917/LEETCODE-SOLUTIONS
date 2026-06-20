1d16f0b981360308b30202b1780b922e4206343d
Shubham Yadav <shu940229@gmail.com>
2026-06-21 01:33:05 +0530

    Time: 24 ms (12.79%), Space: 14.3 MB (28.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,16 +8,15 @@ public:
     
 		while( i<n && j<n)
 		{
-			if(set.find(s[j]) == set.end()) //If the character does not in the set
+			if(set.find(s[j]) == set.end()) 
 			{
-				set.insert(s[j++]); //Insert the character in set and update the j counter
-				ans = max(ans, j-i); //Check if the new distance is longer than the current answer
+				set.insert(s[j++]); 
+				ans = max(ans, j-i); 
 			}
 			else
 			{
 				set.erase(s[i++]); 
-				/*If character does exist in the set, ie. it is a repeated character, 
-				we update the left side counter i, and continue with the checking for substring. */
+				
 			}
 		}
     
