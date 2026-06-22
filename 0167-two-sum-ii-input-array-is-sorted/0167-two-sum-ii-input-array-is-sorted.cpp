8f9b1554582990399488188c16549cc951b1e974
Shubham Yadav <shu940229@gmail.com>
2026-06-22 14:11:38 +0530

    Time: 0 ms (100%), Space: 19.4 MB (64.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -20,22 +20,47 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        for(int i = 0; i < n; i++) {
-            int need = target - numbers[i];
-            int l = i + 1;
-            int r = n - 1;
-            while(l <= r) {
-                int mid = l + (r - l) / 2;
-                if(numbers[mid] == need)
-                    return {i + 1, mid + 1};
-                if(numbers[mid] < need)
-                    l = mid + 1;
-                else
-                    r = mid - 1;
+        int left = 0;
+        int right = numbers.size() - 1;
+        while(left < right) {
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) {
+                return {left + 1, right + 1};
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
             }
         }
-
         return {};
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         int n = numbers.size();
+//         for(int i = 0; i < n; i++) {
+//             int need = target - numbers[i];
+//             int l = i + 1;
+//             int r = n - 1;
+//             while(l <= r) {
+//                 int mid = l + (r - l) / 2;
+//                 if(numbers[mid] == need)
+//                     return {i + 1, mid + 1};
+//                 if(numbers[mid] < need)
+//                     l = mid + 1;
+//                 else
+//                     r = mid - 1;
+//             }
+//         }
+
+//         return {};
+//     }
+// };
+
+
+
